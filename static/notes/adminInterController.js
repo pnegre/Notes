1c4d57fe0c01d35c208976d994c9e8c34441b6c5
pnegre <pere.negre@gmail.com>
2016-09-18 12:13:04 +0200

    Anem fent

--- static/notes/adminInterController.js
+++ static/notes/adminInterController.js
@@ -1,5 +1,20 @@
 
 
 app.controller("adminInterController", function($scope, $http) {
-    $scope.prova = "hello";
+    $scope.anys = [
+        { 'nom': '2015-2016', 'id': 1, 'inters': [
+            {'nom': 'inter1', 'id': 1},
+            {'nom': 'inter2', 'id': 2},
+        ]},
+        { 'nom': '2016-2017', 'id': 2, 'inters': [
+            {'nom': 'inter11', 'id': 3},
+            {'nom': 'inter22', 'id': 4},
+        ]},
+    ];
+
+    $scope.showInters = function() {
+        console.log("Dins showInters");
+        console.log($scope.selectedAny);
+    }
+
 });
