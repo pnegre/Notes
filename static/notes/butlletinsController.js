d7599521f2dd49c4c26377122884f6df11748540
pnegre <pere.negre@gmail.com>
2016-09-24 22:46:12 +0200

    Anem fent

--- static/notes/butlletinsController.js
+++ static/notes/butlletinsController.js
@@ -5,17 +5,23 @@ app.controller("butlletinsController", function($scope, $http) {
         $scope.anys = response.data;
     });
 
+    $scope.changeAny = function() {
+        $scope.link = null;
+        $scope.cursos = null;
+        $scope.selectedInter = null;
+        $scope.selectedCurs = null;
+    }
+
     $scope.showCursos = function() {
         $http.get('/notes/interCursos/' + $scope.selectedInter.id).then(function(response) {
             $scope.cursos = response.data;
+            $scope.link = null;
         });
     }
 
     $scope.prepareLink = function() {
-        $scope.link = '/notes/butlletiPDF/' + $scope.selectedInter.id + '/' + $scope.selectedCurs.id;
-    }
-
-    $scope.baixa = function() {
-        console.log("dins baixa");
+        if ($scope.selectedInter != null && $scope.selectedCurs != null) {
+            $scope.link = '/notes/butlletiPDF/' + $scope.selectedInter.id + '/' + $scope.selectedCurs.id;
+        }
     }
 });
